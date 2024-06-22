2c8ddaa3578e67fc154efe7b6d80d14616d4d878
Ryokan Ri <ryokan.ri@sbintuitions.co.jp>
2024-06-22 18:55:02 +0900

    Deployed 6dd6bbc with MkDocs version: 1.6.0

--- api_reference.py
+++ api_reference.py
@@ -14,6 +14,7 @@ root = Path(__file__).parent.parent
 def _get_all_modules(module_import_path: str) -> list[str]:
     module = importlib.import_module(module_import_path)
     module_classes = inspect.getmembers(module, inspect.isclass)
+    module_classes += inspect.getmembers(module, inspect.isfunction)
     return [cls[1].__module__ + "." + cls[0] for cls in module_classes]
 
 
@@ -28,16 +29,17 @@ for base_py_file_path in sorted((root / "flexeval" / "core").rglob("base.py")):
     base_class_name = next(module for module in all_modules if "base" in module).split(".")[-1]
     all_core_modules[base_class_name] = _get_all_modules(module_import_path)
 
-# collect `EvalSetup` modules from `flexeval/scripts/flexeval_lm.py`
+# collect `EvalSetup` modules from `flexeval/core/eval_setups.py`
 all_core_modules["EvalSetup"] = [
-    module
-    for module in _get_all_modules("flexeval.scripts.flexeval_lm")
-    if module.startswith("flexeval.scripts.flexeval_lm")
+    module for module in _get_all_modules("flexeval.core.eval_setups") if module.startswith("flexeval.core.eval_setups")
 ]
+
+# collect utils
+all_core_modules["utils"] = _get_all_modules("flexeval.utils")
 
 for name, module_classes in sorted(all_core_modules.items()):
     # sort classes so that base classes come first
-    module_classes.sort(key=lambda x: ("base" not in x, x))
+    module_classes.sort(key=lambda x: ("base" not in x and "EvalSetup" not in x, x))
     with mkdocs_gen_files.open(Path("api_reference") / f"{name}.md", "w") as fd:
         for class_path in module_classes:
             class_name = class_path.split(".")[-1]
