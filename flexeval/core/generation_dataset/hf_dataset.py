71cf92313b70f0b734f85b01cb4e3c5e35141049
Ryokan Ri <ryokan.ri@sbintuitions.co.jp>
2024-06-28 16:47:22 +0900

    implement template filters in HFGenerationDataset

--- flexeval/core/generation_dataset/hf_dataset.py
+++ flexeval/core/generation_dataset/hf_dataset.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+import functools
 from ast import literal_eval
+from typing import Any
 
 import datasets
+import jinja2
 from jinja2 import Template
 
 from flexeval.core.utils.jinja2_env import JINJA2_ENV
@@ -20,7 +23,8 @@ class HFGenerationDataset(GenerationDataset):
         references_template: A Jinja2 template for the references.
         input_templates: A dictionary of Jinja2 templates for the inputs.
         subset: The subset of the dataset to use.
-        max_lengths: If provided, filter out instances with lengths exceeding the specified values.
+        template_filters: A dictionary to indicate the condition to filter certain items.
+            The key is a Jinja2 template string to embed the item into a string, and the value is the value to keep.
     """
 
     def __init__(
@@ -30,13 +34,23 @@ class HFGenerationDataset(GenerationDataset):
         references_template: str,
         input_templates: dict[str, str] | None = None,
         subset: str | None = None,
-        max_lengths: dict[str, int] | None = None,
+        template_filters: dict[str, str] | None = None,
     ) -> None:
         self._dataset = datasets.load_dataset(path, name=subset, split=split)
 
-        max_lengths = max_lengths or {}
-        for key, max_length in max_lengths.items():
-            self._dataset = self._dataset.filter(lambda item, k=key, max_l=max_length: len(item[k]) <= max_l)
+        template_filters = template_filters or {}
+        for template_str, value_to_keep in template_filters.items():
+
+            def _to_keep_this_item(item: dict[str, Any], filter_template: jinja2.Template, value_to_keep: str) -> bool:
+                return filter_template.render(**item) == value_to_keep
+
+            self._dataset = self._dataset.filter(
+                functools.partial(
+                    _to_keep_this_item,
+                    filter_template=JINJA2_ENV.from_string(template_str),
+                    value_to_keep=value_to_keep,
+                ),
+            )
 
         input_templates = input_templates or {}
         self._input_templates: dict[str, Template] = {k: JINJA2_ENV.from_string(v) for k, v in input_templates.items()}
--- tests/core/generation_dataset/test_hf_dataset.py
+++ tests/core/generation_dataset/test_hf_dataset.py
@@ -3,9 +3,9 @@ from flexeval.core.generation_dataset import HFGenerationDataset
 
 def test_hf_dataset() -> None:
     dataset = HFGenerationDataset(
-        path="llm-book/aio",
-        split="validation",
-        input_templates={"additional_input": "追加の問題：{{question}}"},
+        path="tests/dummy_modules/hf_dataset",
+        split="train",
+        input_templates={"additional_input": "added_question: {{question}}"},
         references_template="{{ answers }}",
     )
 
@@ -13,17 +13,29 @@ def test_hf_dataset() -> None:
 
     item = dataset[0]
     assert item.inputs == {
-        "answers": ["ジェット団"],
-        "competition": "第2回AI王",
-        "number": 1,
-        "original_additional_info": "",
-        "original_answer": "ジェット団",
-        "original_question": "映画『ウエスト・サイド物語』に登場する2つの少年グループといえば、シャーク団と何団？",
-        "qid": "AIO02-0001",
-        "question": "映画『ウエスト・サイド物語』に登場する2つの少年グループといえば、シャーク団と何団?",
-        "section": "開発データ問題",
-        "timestamp": "2021/01/29",
-        "additional_input": "追加の問題：映画『ウエスト・サイド物語』に登場する2つの少年グループといえば、シャーク団と何団?",  # noqa: E501
+        "question": "What is the highest mountain in the world.",
+        "answers": ["Mount Everest", "Everest"],
+        "additional_input": "added_question: What is the highest mountain in the world.",
     }
+    assert item.references == ["Mount Everest", "Everest"]
 
-    assert item.references == ["ジェット団"]
+
+def test_test_template_filters() -> None:
+    original_dataset = HFGenerationDataset(
+        path="tests/dummy_modules/hf_dataset",
+        split="train",
+        references_template="",
+    )
+
+    filtered_dataset = HFGenerationDataset(
+        path="tests/dummy_modules/hf_dataset",
+        split="train",
+        references_template="",
+        template_filters={
+            "{{ answers | length }}": "1",
+        },
+    )
+
+    assert 0 < len(filtered_dataset) < len(original_dataset)
+    for item in filtered_dataset:
+        assert len(item.inputs["answers"]) == 1
