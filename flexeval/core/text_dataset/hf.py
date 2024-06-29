bb875126c6edd1c276f4580604b07a98eaeadda0
Ryokan Ri <ryokan.ri@sbintuitions.co.jp>
2024-06-29 11:42:47 +0900

    update HFTextDataset

--- flexeval/core/text_dataset/hf.py
+++ flexeval/core/text_dataset/hf.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import datasets
 
+from flexeval.core.utils.jinja2_utils import JINJA2_ENV, get_template_filter_function
+
 from .base import TextDataset
 
 
@@ -12,18 +14,31 @@ class HFTextDataset(TextDataset):
     Args:
         path: The name of the dataset to load.
         split: The split of the dataset to load.
-        field: The field to extract from the dataset.
+        text_template: A Jinja2 template for the text.
         subset: The subset of the dataset to load.
+        template_filters: A dictionary to indicate the condition to filter certain items.
+            The key is a Jinja2 template string to embed the item into a string, and the value is the value to keep.
     """
 
-    def __init__(self, path: str, split: str, field: str, subset: str | None = None) -> None:
-        self._dataset: list[str] = datasets.load_dataset(path, split=split, name=subset)[field]
-        if not isinstance(self._dataset[0], str):
-            msg = f"field '{field}' is not string type"
-            raise TypeError(msg)
+    def __init__(
+        self,
+        path: str,
+        split: str,
+        text_template: str,
+        subset: str | None = None,
+        template_filters: dict[str, str] | None = None,
+    ) -> None:
+        self._dataset = datasets.load_dataset(path, split=split, name=subset)
+
+        template_filters = template_filters or {}
+        for template_str, value_to_keep in template_filters.items():
+            self._dataset = self._dataset.filter(get_template_filter_function(template_str, value_to_keep))
+
+        self._text_template = JINJA2_ENV.from_string(text_template)
 
     def __len__(self) -> int:
         return len(self._dataset)
 
-    def __getitem__(self, item: int) -> str:
-        return self._dataset[item]
+    def __getitem__(self, i: int) -> str:
+        item = self._dataset[i]
+        return self._text_template.render(**item)
--- tests/core/text_dataset/test_hf.py
+++ tests/core/text_dataset/test_hf.py
@@ -3,11 +3,30 @@ from flexeval.core.text_dataset import HFTextDataset
 
 def test_hf_text_dataset() -> None:
     dataset = HFTextDataset(
-        path="llm-book/aio",
-        split="validation[:10]",
-        field="question",
+        path="tests/dummy_modules/hf_dataset",
+        split="train",
+        text_template="{{ question }}",
     )
 
     texts = list(dataset)
     assert len(texts) == 10
     assert isinstance(texts[0], str)
+
+
+def test_test_template_filters() -> None:
+    original_dataset = HFTextDataset(
+        path="tests/dummy_modules/hf_dataset",
+        split="train",
+        text_template="{{ question }}",
+    )
+
+    filtered_dataset = HFTextDataset(
+        path="tests/dummy_modules/hf_dataset",
+        split="train",
+        text_template="{{ question }}",
+        template_filters={
+            "{{ answers | length }}": "1",
+        },
+    )
+
+    assert 0 < len(filtered_dataset) < len(original_dataset)
