e62d495c18bdc6c191937f703506b9eb986999f2
Koga Kobayashi <koga.kobayashi@sbintuitions.co.jp>
2024-09-09 18:09:15 +0900

    Add OpenAIChatBatchAPI

--- flexeval/core/language_model/__init__.py
+++ flexeval/core/language_model/__init__.py
@@ -1,4 +1,5 @@
 from .base import LanguageModel
 from .hf_lm import HuggingFaceLM
 from .openai_chatgpt import OpenAIChatAPI
+from .openai_chatgpt_batch_api import OpenAIChatBatchAPI
 from .vllm_model import VLLM
