5efa9fb748c920df015366c65565998f09f9a0af
Ryokan Ri <ryokan.ri@sbintuitions.co.jp>
2025-01-07 10:19:28 +0900

    remove NoopNormalizer

--- flexeval/core/metric/string_processor/__init__.py
+++ flexeval/core/metric/string_processor/__init__.py
@@ -3,6 +3,5 @@ from .base import StringProcessor
 from .last_line import LastLineExtractor
 from .lower import StringLower
 from .nfkc import NFKCNormalizer
-from .noop import NoopNormalizer
 from .regex import RegexExtractor
 from .string_strip import StringStrip
