0368125fe2d9a630499a73950de12219e4a11d53
Ryokan Ri <ryokan.ri@sbintuitions.co.jp>
2025-03-01 16:04:47 +0900

    Add TransformersTokenizer

--- flexeval/core/metric/tokenizer/__init__.py
+++ flexeval/core/metric/tokenizer/__init__.py
@@ -2,4 +2,5 @@ from .base import Tokenizer
 from .mecab import MecabTokenizer
 from .sacrebleu_tokenizer import SacreBleuTokenizer
 from .tiktoken_tokenizer import TiktokenTokenizer
+from .transformers_tokenizer import TransformersTokenizer
 from .whitespace import WhitespaceTokenizer
