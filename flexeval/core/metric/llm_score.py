6158a2da4d446ef8d026d3f01dda3ae4cc052dfa
Masato Umakoshi <masato.umakoshi@sbintuitions.co.jp>
2025-03-06 20:09:49 +0900

    Handle category with list[str] in LLMScore (#154)
    * implement to handle list[str] and tests
    
    * lint
    
    * Update flexeval/core/metric/llm_score.py
    
    Co-authored-by: Ryokan RI <ryokan.ri@sbintuitions.co.jp>
    
    * bug fix
    
    ---------
    
    Co-authored-by: Ryokan RI <ryokan.ri@sbintuitions.co.jp>

--- flexeval/core/metric/llm_score.py
+++ flexeval/core/metric/llm_score.py
@@ -49,7 +49,11 @@ def summarize_evaluator_scores(
         if score is None or category_key is None:
             continue
         if category_key in task_inputs:
-            category2valid_scores[task_inputs[category_key]].append(score)
+            categories = task_inputs[category_key]
+            if not isinstance(categories, (list, tuple, set)):
+                categories = [categories]
+            for category in categories:
+                category2valid_scores[category].append(score)
 
     category2mean_score: dict[str, float] = {}
     for category, valid_scores in category2valid_scores.items():
