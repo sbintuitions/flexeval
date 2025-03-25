493bf26a43a253f1038700cd6212a97468253821
Ryokan Ri <ryokan.ri@sbintuitions.co.jp>
2025-03-25 09:37:01 +0900

    add loose_jinja

--- flexeval/core/utils/jinja2_utils.py
+++ flexeval/core/utils/jinja2_utils.py
@@ -3,14 +3,14 @@ from __future__ import annotations
 import re
 
 import jinja2
-from jinja2 import sandbox
+from jinja2 import Environment
 
 
 def regex_replace(s: str, pattern: str, replace: str) -> str:
     return re.sub(pattern, replace, s)
 
 
-JINJA2_ENV = sandbox.ImmutableSandboxedEnvironment(
+JINJA2_ENV = Environment(
     undefined=jinja2.StrictUndefined,
     autoescape=False,  # important for not escaping double quotations
     keep_trailing_newline=True,
