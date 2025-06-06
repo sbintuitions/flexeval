c62e17e58892fb376a2d2ccbbfcb805705b467a2
yuma-hirakawa <yuma.hirakawa@sbintuitions.co.jp>
2025-06-06 11:16:52 +0900

    fix format error

--- flexeval/core/evaluate_multiple_choice.py
+++ flexeval/core/evaluate_multiple_choice.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from typing import Any, Sequence
-from sklearn.metrics import f1_score
+from collections.abc import Sequence
+from typing import Any
 
 from loguru import logger
+from sklearn.metrics import f1_score
 from tqdm import tqdm
 
 from .few_shot_generator import FewShotGenerator
@@ -58,7 +59,7 @@ def evaluate_multiple_choice(
             if batch_id == 0:
                 logger.info("Example of the model inputs and outputs:")
                 logger.info(f"prefix: {batch_prefixes[0]}")
-                logger.info(f"choices: {batch_choices[:len(eval_instance.choices)]}")
+                logger.info(f"choices: {batch_choices[: len(eval_instance.choices)]}")
 
             batch_log_probs = language_model.compute_log_probs(
                 text_list=batch_choices,
