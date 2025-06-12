69826903cadd7d148a875d5087868d214e9f8ee6
Masato Umakoshi <masato.umakoshi@sbintuitions.co.jp>
2025-06-12 14:26:08 +0900

    feat: implement

--- flexeval/core/chat_dataset/__init__.py
+++ flexeval/core/chat_dataset/__init__.py
@@ -1,4 +1,5 @@
 from .base import ChatDataset, ChatInstance
 from .chatbot_bench import ChatbotBench
+from .openai_messages import OpenAIMessagesDataset
 from .sacrebleu_dataset import SacreBleuChatDataset
 from .template_based import HFChatDataset, JsonlChatDataset, TemplateChatDataset
---
+++ flexeval/core/chat_dataset/openai_messages.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import json
+from typing import Iterator
+
+from .base import ChatDataset, ChatInstance
+
+
+def read_lines_from_file(file_path: str) -> Iterator[str]:
+    with open(file_path, encoding="utf-8") as fi:
+        yield from fi
+
+
+class OpenAIMessagesDataset(ChatDataset):
+    """This class loads data with OpenAI-like format in jsonl file.
+    The difference lies in that this class has 'tool_definition' field, in which
+    available tools are listed.
+
+    Parameters:
+        file_path (str | list[str] | None): Path or list of paths to `.jsonl` file(s).
+        message_key (str): Key used to extract the list of messages from each JSON object.
+        tool_definitions_key (str | None): Key used to extract the list of tool definitions from each JSON object.
+            Set to `None` (default) for data without tool_calls.
+
+    In Jsonl, each line must have a following structure:
+
+    {
+      '<message_key>': [
+        {
+          'role': 'user',
+          'content': 'こんにちわ。元気になる言葉を教えて下さい。'
+        },
+        {
+          'role': 'assistant',
+          'content': 'こんなのはどうでしょう。どんどんやってください！'
+        }
+      ],
+      '<tool_definitions_key>': [
+        {
+          'type': 'function',
+          'function': { ... }
+        }
+      ]
+    }
+    """
+
+    def __init__(
+        self,
+        file_path: str | None = None,
+        message_key: str = "messages",
+        tool_definitions_key: str | None = None,
+    ) -> None:
+        self.conversations: list[ChatInstance] = []
+        with open(file_path) as f:
+            dataset = [json.loads(line) for line in f]
+        for sample in dataset:
+            tool_dicts = None
+            if tool_definitions_key is not None:
+                tool_dicts = sample.get(tool_definitions_key, None)
+
+            self.conversations.append(ChatInstance(messages=sample[message_key], tools=tool_dicts))
+
+    def __len__(self) -> int:
+        return len(self.conversations)
+
+    def __getitem__(self, idx: int) -> ChatInstance:
+        return self.conversations[idx]
