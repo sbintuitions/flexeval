ee7820d5a1674d96c2eff535c4ff62759355795f
Takuya Kato <17094694+Ktakuya332C@users.noreply.github.com>
2025-11-12 13:47:17 +0900

    Add truncate_middle to jinja env

--- tests/core/utils/test_jinja2_env.py
+++ tests/core/utils/test_jinja2_env.py
@@ -6,3 +6,10 @@ from flexeval.core.utils.jinja2_utils import JINJA2_ENV
 def test_regex_replace() -> None:
     template = "{{ text | regex_replace('<<.*?>>', '') }}"
     assert JINJA2_ENV.from_string(template).render(text="<<a>>Hello <<dummy>>world!") == "Hello world!"
+
+
+def test_truncate_middle() -> None:
+    s = "This is a pen"  # len(s) = 13
+    assert truncate_middle(s, length=14, filler="+") == "This is a pen"
+    assert truncate_middle(s, length=13, filler="+") == "This i+ a pen"
+    assert truncate_middle(s, length=12, filler="+") == "This + a pen"
