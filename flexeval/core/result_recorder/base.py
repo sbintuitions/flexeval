44d4fe2f87b11ca703db1bd84ec926475b9e0716
junya-takayama <clps1220@gmail.com>
2025-11-21 21:52:27 +0900

    fix

--- flexeval/core/result_recorder/base.py
+++ flexeval/core/result_recorder/base.py
@@ -56,7 +56,6 @@ class ResultRecorder(ABC):
         """
         return False
 
-    @abstractmethod
     def is_outputs_saved(self, group: str | None = None) -> bool:
         """
         Check if the model outputs have already been saved.
@@ -66,7 +65,6 @@ class ResultRecorder(ABC):
         """
         return False
 
-    @abstractmethod
     def is_metrics_saved(self, group: str | None = None) -> bool:
         """
         Check if the evaluation metrics have already been saved.
